40f38348afee3bb39511d159e924f5bd3d5624b2
DafniMilla <dafni8663@gmail.com>
2025-05-11 00:01:26 -0300

    projeto react1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  let [lista, setLista] = useState([]);
+  let [novoItem, setNovoItem] = useState("");
+
+  useEffect(() => {
+  setLista(["Ler", "Estudar matemática", "Consulta",]);
+
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='titulo'>Lista de tarefas</h1>
+      <div className='inputBotao'>
+        <input id='input' placeholder="tarefa" value={novoItem} onChange={value => setNovoItem(value.target.value)} type="text" />
+        <button className='btn' onClick={() => adicionarItem()}>Adicionar </button>
+      </div>
+      <ul className='lista'>
+        {lista.map((item, index) => (
+          <li key={index} className='item'>
+            {item}
+            <button className='btnDelete' onClick={() => deletarItem(index)}>
+              Deletar
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
+
+  function adicionarItem() {
+    if (novoItem.length <= 0) {
+      alert("Digite algo no campo 'tarefa'")
+      return;
+    }
+
+    let itemIndex = lista.indexOf(novoItem);
+    if (itemIndex >= 0) {
+      alert("Voçê já adicionou esta tarefa.");
+      return;
+    }
+
+    setLista([...lista, novoItem]);
+    setNovoItem("")
+  }
+  function deletarItem(index) {
+    let tmpArray = [...lista];
+    tmpArray.splice(index, 1);
+
+    setLista(tmpArray);
+  }
 }
 
 export default App;
